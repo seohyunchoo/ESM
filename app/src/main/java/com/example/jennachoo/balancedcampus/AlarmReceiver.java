47deda6e2e4e195514b675cf9c05c732f90a2d93
jennachoo <seohyunchoo@gmail.com>
2015-07-15 12:23:00 -0400

    got rid of timertask, used alarmManager in alarmReceiver

--- app/src/main/java/com/example/jennachoo/balancedcampus/AlarmReceiver.java
+++ app/src/main/java/com/example/jennachoo/balancedcampus/AlarmReceiver.java
@@ -1,13 +1,17 @@
 package com.example.jennachoo.balancedcampus;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Vibrator;
-
+import android.util.Log;
 
 
 import com.aware.ESM;
+
+import java.util.Calendar;
 
 public class AlarmReceiver extends BroadcastReceiver{
     private static final String ONE = "{'esm':{" +
@@ -48,8 +52,32 @@ public class AlarmReceiver extends BroadcastReceiver{
             "}}";
 
     private static final String MORNINGJSON = "[" + ONE + "," + TWO + "," +  THREE + "," +  FOUR + "]";
+    private PendingIntent nextIntent;
+    private AlarmManager alarmManager;
 
     public AlarmReceiver() {
+    }
+
+    public void scheduleNextQuestionnaire(Context context) {
+        Intent alarmIntent = new Intent(context, AlarmReceiver.class);
+        Calendar cal = Calendar.getInstance();
+        if(cal.get(Calendar.HOUR_OF_DAY) < 20) {
+            cal.add(Calendar.DAY_OF_YEAR, 0);
+            cal.add(Calendar.HOUR_OF_DAY, 4);
+            cal.set(Calendar.MINUTE,00);
+            cal.set(Calendar.SECOND, 00);
+            Log.d("BALANCEDCAMPUS", "Same day " + cal.get(Calendar.HOUR_OF_DAY));
+        } else {
+            cal.add(Calendar.DAY_OF_YEAR,1);
+            cal.set(Calendar.HOUR_OF_DAY,9);
+            cal.set(Calendar.MINUTE, 00);
+            cal.set(Calendar.SECOND,00);
+            Log.d("BALANCEDCAMPUS", "Next day " + cal.get(Calendar.HOUR_OF_DAY));
+        }
+        nextIntent = PendingIntent.getBroadcast(context, 123124,
+                alarmIntent, PendingIntent.FLAG_CANCEL_CURRENT);
+        alarmManager.setExact(AlarmManager.RTC_WAKEUP, cal.getTimeInMillis(), nextIntent);
+        Log.d("ALARMRECEIVER", "Next Alarm:" + cal.getTimeInMillis());
     }
 
     @Override
@@ -61,5 +89,7 @@ public class AlarmReceiver extends BroadcastReceiver{
         String esmJSON  = MORNINGJSON;
         queue_esm.putExtra(ESM.EXTRA_ESM, esmJSON);
         context.sendBroadcast(queue_esm);
+        alarmManager = (AlarmManager)context.getSystemService(Context.ALARM_SERVICE);
+        scheduleNextQuestionnaire(context);
     }
 }
--- app/src/main/java/com/example/jennachoo/balancedcampus/Plugin.java
+++ app/src/main/java/com/example/jennachoo/balancedcampus/Plugin.java
@@ -6,11 +6,6 @@ import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import android.content.IntentFilter;
-import android.database.Cursor;
-import android.os.Build;
-import android.os.IBinder;
-import android.os.Binder;
 import android.util.Log;
 import com.aware.Aware;
 import com.aware.Aware_Preferences;
@@ -27,9 +22,6 @@ import java.util.TimerTask;
 public class Plugin extends Aware_Plugin {
     private AlarmManager alarmManager;
     private PendingIntent morningIntent = null;
-    private PendingIntent noonIntent = null;
-    private PendingIntent afternoonIntent = null;
-    private PendingIntent eveningIntent = null;
 
     @Override
     public void onCreate() {
@@ -38,50 +30,18 @@ public class Plugin extends Aware_Plugin {
         DEBUG = Aware.getSetting(this, Aware_Preferences.DEBUG_FLAG).equals("true");
     }
 
-    Timer timer = new Timer ();
-    TimerTask dailyTask = new TimerTask () {
-        @Override
-        public void run () {
-            Log.d(TAG,"rescheduled");
-            scheduleQuestionnaire();
-        }
-    };
 
-
-    @TargetApi(Build.VERSION_CODES.KITKAT)
     public void scheduleQuestionnaire() {
-        Intent alarmIntent = new Intent(this, AlarmReceiver.class);
+        Intent alarmIntent = new Intent(this,AlarmReceiver.class);
         Calendar cal = Calendar.getInstance();
-        //cal.add(Calendar.HOUR_OF_DAY, 1);
-        cal.add(Calendar.MINUTE, 3);
+        cal.add(Calendar.DAY_OF_YEAR, 0);
+        cal.set(Calendar.HOUR_OF_DAY, 17);
+        cal.set(Calendar.MINUTE, 00);
         cal.set(Calendar.SECOND, 00);
-        Calendar cal2 = Calendar.getInstance();
-        //cal2.add(Calendar.HOUR_OF_DAY, 1);
-        cal2.add(Calendar.MINUTE, 8);
-        cal2.set(Calendar.SECOND, 00);
-        Calendar cal3 = Calendar.getInstance();
-        //cal3.add(Calendar.HOUR_OF_DAY, 0);
-        cal3.add(Calendar.MINUTE, 13);
-        cal3.set(Calendar.SECOND, 00);
-        Calendar cal4 = Calendar.getInstance();
-        cal4.add(Calendar.HOUR_OF_DAY, 1);
-        cal4.set(Calendar.MINUTE, 00);
-        cal4.set(Calendar.SECOND,00);
-
-        morningIntent = PendingIntent.getBroadcast(getApplicationContext(), 123123, alarmIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-        noonIntent = PendingIntent.getBroadcast(getApplicationContext(),123124, alarmIntent,PendingIntent.FLAG_UPDATE_CURRENT);
-        afternoonIntent = PendingIntent.getBroadcast(getApplicationContext(),123125, alarmIntent,PendingIntent.FLAG_UPDATE_CURRENT);
-       //eveningIntent = PendingIntent.getBroadcast(getApplicationContext(),123126, alarmIntent,PendingIntent.FLAG_UPDATE_CURRENT);
-
+        morningIntent = PendingIntent.getBroadcast(getApplicationContext(),123123,
+                alarmIntent, PendingIntent.FLAG_CANCEL_CURRENT);
         alarmManager.setExact(AlarmManager.RTC_WAKEUP, cal.getTimeInMillis(), morningIntent);
-        alarmManager.setExact(AlarmManager.RTC_WAKEUP, cal2.getTimeInMillis(), noonIntent);
-        alarmManager.setExact(AlarmManager.RTC_WAKEUP, cal3.getTimeInMillis(), afternoonIntent);
-        //alarmManager.setExact(AlarmManager.RTC_WAKEUP, cal4.getTimeInMillis(), eveningIntent);
-
         Log.d(TAG, "Alarm 1 :" + cal.getTimeInMillis());
-        Log.d(TAG, "Alarm 2 :" + cal2.getTimeInMillis());
-        Log.d(TAG, "Alarm 3 :" + cal3.getTimeInMillis());
-       // Log.d(TAG, "Alarm 4 :" + cal4.getTimeInMillis());
     }
 
     @Override
@@ -94,13 +54,12 @@ public class Plugin extends Aware_Plugin {
 
     @Override
     public int onStartCommand(Intent intent, int x, int y){
-        super.onStartCommand(intent,x,y);
-        Log.d(TAG,"service started");
+        super.onStartCommand(intent, x, y);
+        Log.d(TAG, "service started");
         Aware.setSetting(getApplicationContext(), Aware_Preferences.STATUS_ESM, true);
-        //Aware.setSetting(getApplicationContext(), Aware_Preferences.STATUS_SCREEN, true);
         if (DEBUG) Log.d(TAG, "Balanced Campus ESM running");
-        alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
-        timer.scheduleAtFixedRate(dailyTask, 0, 1000*60*15);
+        alarmManager = (AlarmManager)getApplicationContext().getSystemService(Context.ALARM_SERVICE);
+        scheduleQuestionnaire();
         return 1;
     }
 }
